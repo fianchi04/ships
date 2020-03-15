c9b1840ee731c4664c728d4ceb8eed7e7d1d1dca
A Wilde <av.wilde@hotmail.co.uk>
2020-03-15 19:30:56 +0000

    added db config

---
+++ src/main/java/com/shipping/ships/service/domain/Ship.java
@@ -0,0 +1,12 @@
+package com.shipping.ships.service.domain;
+
+public class Ship {
+//    private final int id;
+//    private final int built;
+//    private final String name;
+//    private final double lengthMeters;
+//    private final double beamMeters;
+//    private final int maxTEU;
+//    private final String owner;
+//    private final int grossTonnage;
+}
